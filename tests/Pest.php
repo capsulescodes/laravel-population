5cc510817bca926e0ef9eac2a9d960f5f4d638ba
mho22 <yannick@capsules.codes>
2024-06-08 12:14:15 +0200

    Added grouping for test cases to make github actions work

--- tests/Pest.php
+++ tests/Pest.php
@@ -6,7 +6,7 @@ use CapsulesCodes\Population\Tests\Cases\SQLiteTestCase;
 use CapsulesCodes\Population\Tests\Cases\TestCase;
 
 
-uses( TestCase::class )->in( 'Unit/*.php', 'Feature/*.php' );
-uses( SQLiteTestCase::class )->in( 'Unit/SQLite', 'Feature/SQLite' );
+uses( TestCase::class )->group( 'continuous-integration' )->in( 'Unit/*.php', 'Feature/*.php' );
+uses( SQLiteTestCase::class )->group( 'continuous-integration' )->in( 'Unit/SQLite', 'Feature/SQLite' );
 uses( MySQLTestCase::class )->in( 'Unit/MySQL', 'Feature/MySQL' );
 uses( MariaDBTestCase::class )->in( 'Unit/MariaDB', 'Feature/MariaDB' );
